b88d1614ebbe6cbf0bebb0365ac0f212a41e3b52
Ravil Tuychin <tuychin@gmail.com>
2021-05-16 23:56:02 +0300

    feat(Button): new props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import cn from 'classnames';
 
 import style from './Button.module.scss';
 
 interface IButtonProps {
+  color?: 'primary' | 'secondary';
+  size?: 'default' | 'small';
+  isFullWidth?: boolean;
   onClick: (evt: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<IButtonProps> = ({ children, onClick }) => {
+const Button: React.FC<IButtonProps> = ({
+  color = 'primary',
+  size = 'default',
+  isFullWidth = false,
+  children,
+  onClick,
+}) => {
   return (
-    <button className={style.root} type="button" onClick={onClick}>
+    <button
+      className={cn(style.root, style[color], style[size], isFullWidth && style.fullWidth)}
+      type="button"
+      onClick={onClick}>
       {children}
     </button>
   );
