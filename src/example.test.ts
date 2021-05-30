55ebbe2b43cbb3cbe5b6b3891e0ec6c7b871f0ac
Ravil Tuychin <tuychin@gmail.com>
2021-05-30 14:44:48 +0300

    jest installed

---
+++ src/example.test.ts
@@ -0,0 +1,11 @@
+describe('Example test', () => {
+  test('Должен вернуть 2 при сложении 1 и 1', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  describe('Example test', () => {
+    test('Должен вернуть 2 при сложении 1 и 1', () => {
+      expect(1 + 1).toBe(2);
+    });
+  });
+});
