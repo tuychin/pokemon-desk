af6012535bdde8570b66f7e4a2728852e61c09e0
Ravil Tuychin <tuychin@gmail.com>
2021-06-02 23:46:44 +0300

    ssr generate index

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import handlebars from 'handlebars';
 import Hapi from '@hapi/hapi';
 import React from 'react';
 import ReactDom from 'react-dom/server';
 import { setPath } from 'hookrouter';
 import App from '../App';
+
+const inert = require('@hapi/inert');
 
 const init = async () => {
   const server = Hapi.server({
@@ -10,12 +15,28 @@ const init = async () => {
     host: 'localhost',
   });
 
+  await server.register(inert);
+
+  server.route({
+    method: 'GET',
+    path: '/main.js',
+    handler: (request, h) => h.file(path.join(process.cwd(), 'dist', 'main.js')),
+  });
+
   server.route({
     method: 'GET',
     path: '/{any*}',
     handler: (request) => {
       setPath(request.path);
-      return ReactDom.renderToString(<App />);
+      const pathIndexHTML = path.join(process.cwd(), 'dist', 'index.html');
+      const template = handlebars.compile(fs.readFileSync(pathIndexHTML, 'utf8'));
+      const component = ReactDom.renderToString(<App />);
+
+      const page = template({
+        content: component,
+      });
+
+      return page;
     },
   });
 
--- webpack.client.config.js
+++ webpack.client.config.js
@@ -14,10 +14,10 @@ module.exports = {
     filename: 'main.js',
   },
   // watch: true,
-  watchOptions: {
-    ignored: /node_modules/,
-    poll: 1000,
-  },
+  // watchOptions: {
+  //   ignored: /node_modules/,
+  //   poll: 1000,
+  // },
   module: {
     rules: [
       {
