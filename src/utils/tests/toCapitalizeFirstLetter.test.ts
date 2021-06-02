c857df28396d40e9c5eeb7b99179a0dd71cecf3a
Ravil Tuychin <tuychin@gmail.com>
2021-06-02 20:57:57 +0300

    feat(toCapitalizeFirstLetter): added new test

--- src/utils/tests/toCapitalizeFirstLetter.test.ts
+++ src/utils/tests/toCapitalizeFirstLetter.test.ts
@@ -4,4 +4,8 @@ describe('toCapitalizeFirstLetter', () => {
   test('Должна сделать первую букву строки заглавной', () => {
     expect(toCapitalizeFirstLetter('строка с заглавной буквы')).toBe('Строка с заглавной буквы');
   });
+
+  test('Должна вернуть пустую строку, если аргумент пустая строка', () => {
+    expect(toCapitalizeFirstLetter('')).toBe('');
+  });
 });
